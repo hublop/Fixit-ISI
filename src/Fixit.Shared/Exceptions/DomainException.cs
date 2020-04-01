efcb27221a4abca770b5cadd58910c684fcbb10c
Hubert Łopusiński <hubert.lopusinski@unit4.com>
2020-04-01 14:13:07 +0200

    Add shared exceptions

---
+++ src/Fixit.Shared/Exceptions/DomainException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Fixit.Shared.Exceptions
+{
+    public class DomainException : Exception
+    {
+        public string Details { get; set; }
+
+        public DomainException(string message, string details) : base(message)
+        {
+            Details = details;
+        }
+    }
+}
---
+++ src/Fixit.Shared/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace Fixit.Shared.Exceptions
+{
+    public class EntityNotFoundException : DomainException
+    {
+        public EntityNotFoundException(string entityName, int id)
+            : base("Object not found.", $"{entityName} with id: {id} has not been found.")
+        {
+        }
+    }
+}
