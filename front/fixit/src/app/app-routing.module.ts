de46b951292226ec76103cc12fafe6472e9e7588
Mateusz Teteruk <mateusz.teteruk@gmail.com>
2020-04-25 20:33:53 +0200

    Added home page with login feature
    * mocking users database
    * force going to login page before accesing home page
    * prepare routing
    * use bootstrap

--- front/fixit/src/app/app-routing.module.ts
+++ front/fixit/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './auth/login/login.component';
+import { AuthGuard } from './auth/_guards/auth.guard';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
