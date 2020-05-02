459952dc01468c1209676cb13b507273b70b6199
Hubert Łopusiński <hubert.lopusinski@unit4.com>
2020-05-02 18:25:30 +0200

    Add material module

---
+++ src/Fixit.ClientApp/src/app/modules/material/material.module.ts
@@ -0,0 +1,66 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MatTooltipModule,
+    MatInputModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatAutocompleteModule,
+    MatSelectModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatListModule,
+    MatProgressSpinnerModule,
+    MatPaginatorModule,
+    MatDividerModule,
+    MatTabsModule,
+    MatExpansionModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatStepperModule
+  ],
+  exports: [
+    MatTooltipModule,
+    MatInputModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatAutocompleteModule,
+    MatSelectModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatListModule,
+    MatProgressSpinnerModule,
+    MatPaginatorModule,
+    MatDividerModule,
+    MatTabsModule,
+    MatExpansionModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatStepperModule
+  ],
+  declarations: []
+})
+export class MaterialModule { }
