4df9a62f8157b0e73b7028a20e30d8dd1c0bf1c5
Hubert Łopusiński <hubert.lopusinski@unit4.com>
2020-05-02 21:54:13 +0200

    Add categories models

---
+++ src/Fixit.ClientApp/src/app/modules/categories/_models/CategoryInfoForList.ts
@@ -0,0 +1,8 @@
+import { SubcategoryInfoForList } from './SubcategoryInfoForList';
+
+export class CategoryInfoForList {
+    id: number;
+    name: string;
+    description: string;
+    subCategories: SubcategoryInfoForList[];
+}
