db6a29591e6469252639433f1b7bfb499460276a
Hubert Łopusiński <hubert.lopusinski@unit4.com>
2020-05-08 21:05:54 +0200

    Add change password command

---
+++ src/Fixit.Application/Auth/Commands/ChangePassword/ChangePasswordCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Fixit.Application.Common.Exceptions;
+using Fixit.Application.Common.Services.Identity;
+using Fixit.Shared.CQRS;
+using MediatR;
+
+namespace Fixit.Application.Auth.Commands.ChangePassword
+{
+    public class ChangePasswordCommandHandler : ICommandHandler<ChangePasswordCommand>
+    {
+        private readonly IPasswordManager _passwordManager;
+
+        public ChangePasswordCommandHandler(IPasswordManager passwordManager)
+        {
+            _passwordManager = passwordManager;
+        }
+
+        public async Task<Unit> Handle(ChangePasswordCommand request, CancellationToken cancellationToken)
+        {
+            if (!await _passwordManager.IsPasswordValid(request.UserId, request.OldPassword))
+            {
+                throw new UnauthorizedException("Unauthorized", $"User with id: {request.UserId} has been unauthorized");
+            }
+
+            if (!await _passwordManager.UpdatePassword(request.UserId, request.OldPassword, request.OldPassword))
+            {
+                throw new UnauthorizedException("Unauthorized", $"User with id: {request.UserId} has been unauthorized");
+            }
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ src/Fixit.Application/Auth/Commands/ChangePassword/ChangePasswordCommandValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+
+namespace Fixit.Application.Auth.Commands.ChangePassword
+{
+    public class ChangePasswordCommandValidator : AbstractValidator<ChangePasswordCommand>
+    {
+        public ChangePasswordCommandValidator()
+        {
+            RuleFor(x => x.OldPassword)
+                .NotNull()
+                .NotEmpty()
+                .Matches("^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})");
+
+            RuleFor(x => x.NewPassword)
+                .NotNull()
+                .NotEmpty()
+                .Matches("^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})");
+
+            RuleFor(x => x.UserId)
+                .NotNull()
+                .NotEmpty();
+        }
+    }
+}
