266d409a3efa627ef6223d73e6e0af51fb7721a5
Hubert Łopusiński <hubert.lopusinski@unit4.com>
2020-05-08 20:44:41 +0200

    Add password manager

---
+++ src/Fixit.Application/Common/Services/Identity/IPasswordManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace Fixit.Application.Common.Services.Identity
+{
+    public interface IPasswordManager
+    {
+        Task<bool> IsPasswordValid(int userId, string password);
+        Task<bool> UpdatePassword(int userId, string oldPassword, string newPassword);
+    }
+}
