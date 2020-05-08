d027691963d9344b82752e8088e39aa3180dff13
Hubert Łopusiński <hubert.lopusinski@unit4.com>
2020-05-08 21:06:37 +0200

    Add login by refresh token command

---
+++ src/Fixit.Application/Auth/Commands/LoginByRefreshToken/LoginByRefreshTokenCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Fixit.Application.Common.Exceptions;
+using Fixit.Application.Common.Services.Identity;
+using Fixit.Shared.CQRS;
+
+namespace Fixit.Application.Auth.Commands.LoginByRefreshToken
+{
+    public class LoginByRefreshTokenCommandHandler : ICommandHandler<LoginByRefreshTokenCommand, JsonWebToken>
+    {
+        private readonly IJwtProvider _jwtProvider;
+        private readonly IRefreshTokenProvider _refreshTokenProvider;
+
+        public LoginByRefreshTokenCommandHandler(IJwtProvider jwtProvider, IRefreshTokenProvider refreshTokenProvider)
+        {
+            _jwtProvider = jwtProvider;
+            _refreshTokenProvider = refreshTokenProvider;
+        }
+
+        public async Task<JsonWebToken> Handle(LoginByRefreshTokenCommand request, CancellationToken cancellationToken)
+        {
+            var isTokeValid = await _refreshTokenProvider.ValidateRefreshTokenAsync(request.UserId, request.RefreshToken);
+
+            if (!isTokeValid)
+            {
+                throw new UnauthorizedException("Unauthorized", $"User with id: {request.UserId} has been unauthorized");
+            }
+
+            var token = await _jwtProvider.GenerateAccessAndRefreshTokenAsync(request.UserId, request.RefreshToken);
+
+            if (token == null)
+            {
+                throw new UnauthorizedException("Unauthorized", $"User with id: {request.UserId} has been unauthorized");
+            }
+
+            return token;
+        }
+    }
+}
