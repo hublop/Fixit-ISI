ad3b8c473b7a0e11a830287acbd9a4db79a45c6d
Michal Bolka <mbolka@divante.pl>
2020-05-09 22:23:42 +0200

    Hotfix: fix passwords

--- setup.sh
+++ setup.sh
@@ -11,6 +11,6 @@ docker-compose build dotnet
 docekr-compose build frontend
 docker-compose up -d
 echo "Waiting until mysql boots up"
-sleep 10
+sleep 15
 docker-compose exec -u www-data php-fpm composer install
 docker-compose exec -u www-data php-fpm bin/console doctrine:migrations:migrate --no-interaction
