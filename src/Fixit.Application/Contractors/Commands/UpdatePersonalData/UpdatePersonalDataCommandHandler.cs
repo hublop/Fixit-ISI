47ae9cff41e7dd9768d73364132d613d1f1f2ae2
Hubert Łopusiński <hubert.lopusinski@unit4.com>
2020-05-12 13:12:22 +0200

    Add update contractor personal data

---
+++ src/Fixit.Application/Contractors/Commands/UpdatePersonalData/UpdatePersonalDataCommandHandler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using Fixit.Application.Common.Interfaces;
+using Fixit.Application.Contractors.Exceptions;
+using Fixit.Domain.Entities;
+using Fixit.Shared.CQRS;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fixit.Application.Contractors.Commands.UpdatePersonalData
+{
+    public class UpdatePersonalDataCommandHandler : ICommandHandler<UpdatePersonalDataCommand>
+    {
+        private readonly IFixitDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public UpdatePersonalDataCommandHandler(IFixitDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public async Task<Unit> Handle(UpdatePersonalDataCommand request, CancellationToken cancellationToken)
+        {
+            var contractor = await _dbContext.Contractors
+                .Include(x => x.Location)
+                .FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken: cancellationToken);
+
+            if (contractor == null)
+            {
+                throw new ContractorDoesNotExistException(request.Id);
+            }
+
+            _mapper.Map(request, contractor);
+
+            if (contractor.Location != null)
+            {
+                contractor.Location.PlaceId = request.PlaceId;
+            }
+            else
+            {
+                contractor.Location = new Location();
+                contractor.Location.PlaceId = request.PlaceId;
+            }
+
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
