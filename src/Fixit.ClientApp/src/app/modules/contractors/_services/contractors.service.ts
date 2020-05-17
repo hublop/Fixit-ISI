03dec0dc3c8eab4f993a74901134bb6f4ba155ee
Mateusz Teteruk <mateusz.teteruk@gmail.com>
2020-05-17 15:24:21 +0200

    add more endpoints in contractors service

--- src/Fixit.ClientApp/src/app/modules/contractors/_services/contractors.service.ts
+++ src/Fixit.ClientApp/src/app/modules/contractors/_services/contractors.service.ts
@@ -2,6 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment } from '../../../../environments/environment';
 import { RegisterContractorData } from '../_models/RegisterContractorData';
+import { ContractorsListFilter } from '../_models/ContractorsListFilter';
+import { Observable } from 'rxjs';
+import { PaginatedResult } from '../../shared/pagination/PaginatedResult';
+import { ContractorForList } from '../_models/ContractorForList';
+import { appendPaginationParams, getPaginationFromResponse } from '../../shared/pagination/Pagination';
+import { map } from 'rxjs/operators';
+import { ContractorProfile } from '../_models/ContractorProfile';
 
 @Injectable({
   providedIn: 'root'
@@ -16,5 +23,44 @@ export class ContractorsService {
 
   register(registerData: RegisterContractorData) {
     return this.http.post(this.baseUrl, registerData);
+  }
+
+  getContractorProfile(id): Observable<ContractorProfile> {
+    return this.http.get<ContractorProfile>(this.baseUrl + id);
+  }
+  
+  getContractors(contractorsListFilter?: ContractorsListFilter, page?, itemsPerPage?): Observable<PaginatedResult<ContractorForList>> {
+    let params: HttpParams = new HttpParams();
+    params = appendPaginationParams(params, page, itemsPerPage);
+    params = this.appendContractorsListFilterParams(params, contractorsListFilter);
+
+    return this.http.get<any>(this.baseUrl, {observe: 'response', params: params}).pipe(map(response => {
+      const result: PaginatedResult<ContractorForList> = {
+        result: response.body,
+        pagination: getPaginationFromResponse(response)
+      };
+
+      return result;
+    }));
+  }
+
+  appendContractorsListFilterParams(params: HttpParams, contractorsListFilter?: ContractorsListFilter): HttpParams {
+    if (!contractorsListFilter) {
+      return params;
+    }
+
+    if (contractorsListFilter.placeId) {
+      params = params.append('placeId', contractorsListFilter.placeId.toString());
+    }
+
+    if (contractorsListFilter.nameSearchString) {
+      params = params.append('nameSearchString', contractorsListFilter.nameSearchString);
+    }
+
+    if (contractorsListFilter.subcategoryId) {
+      params = params.append('subcategoryId', contractorsListFilter.subcategoryId.toString());
+    }
+
+    return params;
   }
 }
