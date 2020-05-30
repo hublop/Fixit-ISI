f687796b5dce6f5f754e9e2f0862e876b95d7c37
Pianista28 <jan.borowiak.jb8@gmail.com>
2020-05-30 22:38:51 +0200

    Add base data for getting orders for contractor

---
+++ src/Fixit.Application/Orders/Queries/GetOrdersForContractor/GetOrdersForContractorQuery.cs
@@ -0,0 +1,12 @@
+﻿using Fixit.Shared.CQRS;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fixit.Application.Orders.Queries.GetOrdersForContractor
+{
+  public class GetOrdersForContractorQuery: IQuery<List<OrderOfferForContractor>>
+  {
+    public int ContractorId { get; set; }
+  }
+}
---
+++ src/Fixit.Application/Orders/Queries/GetOrdersForContractor/GetOrdersForContractorQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Fixit.Application.Common.Interfaces;
+using Fixit.Shared.CQRS;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Fixit.Application.Orders.Queries.GetOrdersForContractor
+{
+  public class GetOrdersForContractorQueryHandler : IQueryHandler<GetOrdersForContractorQuery, List<OrderOfferForContractor>>
+  {
+    private readonly IFixitDbContext _dbContext;
+    private readonly IMapper _mapper;
+
+    public GetOrdersForContractorQueryHandler(IMapper mapper, IFixitDbContext dbContext)
+    {
+      _mapper = mapper;
+      _dbContext = dbContext;
+    }
+
+    public Task<List<OrderOfferForContractor>> Handle(GetOrdersForContractorQuery request, CancellationToken cancellationToken)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
