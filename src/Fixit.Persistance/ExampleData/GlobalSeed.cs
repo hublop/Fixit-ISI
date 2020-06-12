b1ef8dd1ea07f956c99af8d8f595d6e284f5e748
Pianista28 <jan.borowiak.jb8@gmail.com>
2020-06-12 19:04:51 +0200

    Add seed

---
+++ src/Fixit.Persistance/ExampleData/GlobalSeed.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Fixit.Application.Common.Interfaces;
+using Fixit.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace Fixit.Persistance.ExampleData
+{
+    public class GlobalSeed
+    {
+        private readonly IFixitDbContext _context;
+
+        private readonly UserManager<User> _userManager;
+
+
+    public GlobalSeed(IFixitDbContext context, UserManager<User> userManager)
+    {
+        _context = context;
+        _userManager = userManager;
+    }
+
+        public async Task Seed()
+        {
+            CategoriesSeed cs = new CategoriesSeed(_context);
+            await cs.Seed();
+
+            LocationsSeed ls = new LocationsSeed(_context);
+            await ls.Seed();
+
+            ContractorsSeed contractorsSeed = new ContractorsSeed(_context, _userManager);
+            await contractorsSeed.Seed();
+
+            CustomersSeed customersSeed = new CustomersSeed(_context, _userManager);
+            await customersSeed.Seed();
+    }
+    }
+}
---
+++ src/Fixit.Persistance/ExampleData/LocationsSeed.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Fixit.Application.Common.Interfaces;
+using Fixit.Domain.Entities;
+
+namespace Fixit.Persistance.ExampleData
+{
+  public class LocationsSeed
+  {
+      private readonly IFixitDbContext _context;
+
+      public LocationsSeed(IFixitDbContext context)
+      {
+          _context = context;
+      }
+
+      public async Task Seed()
+    {
+        try
+        {
+            if (!_context.Locations.Any())
+            {
+                _context.Locations.AddRange(GetPreconfiguredLocations());
+                await _context.SaveChangesAsync(new CancellationToken());
+            }
+        }
+        catch (Exception e)
+        {
+            
+        }
+    }
+
+      private IEnumerable<Location> GetPreconfiguredLocations()
+      {
+          return new List<Location>
+          {
+              new Location
+              {
+                  PlaceId = "ChIJ3f0CnIjCD0cRQB91xIzugF8",
+                  Latitude = 51.097130,
+                  Longitude = 17.056370
+              },
+              new Location
+              {
+                  PlaceId = "iFQcnVzYSAyMCwgNTAtMzE5IFdyb2PFgmF3LCBQb2xhbmQiMBIuChQKEgn3DSEX2-kPRxEg-UO97j48aBAUKhQKEgkfmu38z-kPRxGTraLc9sudGg",
+                  Latitude = 51.119419,
+                  Longitude = 17.043230
+              },
+              new Location
+              {
+                  PlaceId = "ChIJLyo4-DmYBUcRUcftvj_6Yk4",
+                  Latitude = 51.838430,
+                  Longitude = 16.579950
+              }
+          };
+      }
+  }
+}
